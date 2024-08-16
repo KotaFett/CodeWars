f5da2304d1422d7f9bbb16e65194b134a194ee31
KotaFett <bobafettkot@icloud.com>
2024-08-16 11:23:16 +0500

    added 8 kyu Even or Odd fixed

---
+++ 8 kyu Return the day.py
@@ -0,0 +1,17 @@
+def whatday(num):
+    if num == 1:
+        return "Sunday"
+    if num == 2:
+        return "Monday"
+    if num == 3:
+        return "Tuesday"
+    if num == 4:
+        return "Wednesday"
+    if num == 5:
+        return "Thursday"
+    if num == 6:
+        return "Friday"
+    if num == 7:
+        return "Saturday"
+    if num not in (1, 7):
+        return "Wrong, please enter a number between 1 and 7"
